c42093790104bcb2ce7349daec6d0986752c556b
Anand <anand@eoxys.com>
2017-06-21 17:05:40 +0530

    Sample
    Sample app is a tab controller integrated with.

---
+++ Sample/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Sample
+//
+//  Created by Prabhakar Annavi on 21/06/17.
+//  Copyright © 2017 Eoxys Systems India. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
