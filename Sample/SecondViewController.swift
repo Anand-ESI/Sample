f0255d9ba19af6c1a35bdf7bf4c599346b861ae4
Anand <anand@eoxys.com>
2017-06-21 17:24:39 +0530

    Raja
    Commented as Raja

--- Sample/SecondViewController.swift
+++ Sample/SecondViewController.swift
@@ -12,7 +12,7 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Vignesh")
+        print("Vignesh Raja")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
